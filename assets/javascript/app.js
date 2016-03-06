aace8cfe212875057404a6534415b24838b2e1e8
kg <kjg310@gmail.com>
2016-03-06 13:27:39 -0500

    needed to make it https?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ function render() {
 		$("#gifArea").empty();
 
 		var replace = $(this).attr("data-name");
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + replace + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + replace + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({url: queryURL, method: "GET"}).done(function(gif) {
 			console.log(gif);
